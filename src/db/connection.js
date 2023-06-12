8e9a4cc2f4c4c5a38ab1dfa865de19179e639750
vivektak <v.tak.c@tfoco.com>
2023-06-12 13:45:43 +0530

    feat: update

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const mongoURL = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGODB_URI}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB_NAME}?authSource=admin`;
 
-const mongoURL = `mongodb://${process.env.mongo-user}:${process.env.mongo-password}@${process.env.MONGODB_URI}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB_NAME}?authSource=admin`;
-
+console.log("mongoURL", mongoURL);
 const connectDB = async () => {
-    try {
-        await mongoose.connect(mongoURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connected to mongoDB');
-    } catch (err) {
-        console.log("Error connecting to mongoDB: " + err);
-    }
-}
+  try {
+    await mongoose.connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+  } catch (err) {
+    console.log("Error connecting to mongoDB: " + err);
+  }
+};
 
 module.exports = connectDB;
